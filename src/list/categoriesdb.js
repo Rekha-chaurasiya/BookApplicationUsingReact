21285e5726e1e11ba7441ee5b21e30870652e83c
Rekha-chaurasiya <er.rekhacs@gmail.com>
2024-09-20 11:08:56 +0530

    first commit

---
+++ src/list/categoriesdb.js
@@ -0,0 +1,36 @@
+const categories = [
+   {
+      id: 1,
+      title: "All Books",
+   },
+   {
+      id: 2,
+      title: "Arts & Photography",
+   },
+   {
+      id: 3,
+      title: "Biographies & Memoirs",
+   },
+   {
+      id: 4,
+      title: "Children's Books",
+   },
+   {
+      id: 5,
+      title: "Computers & Technology",
+   },
+   {
+      id: 6,
+      title: "Education & Teaching",
+   },
+   {
+      id: 7,
+      title: "History",
+   },
+   {
+      id: 8,
+      title: "Romance",
+   },
+];
+
+export default categories;
